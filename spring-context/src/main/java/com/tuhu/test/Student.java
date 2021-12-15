5aca69c218c59942b8334ee83e393a5eeded3440
xiaoyang <xiaoyang@tuhu.cn>
2021-12-15 11:34:56 +0800

    添加

---
+++ spring-context/src/main/java/com/tuhu/test/Student.java
@@ -0,0 +1,27 @@
+package com.tuhu.test;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Student {
+
+	private String username = "elephant";
+
+	private String password;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
